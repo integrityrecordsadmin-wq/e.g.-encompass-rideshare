f5bbb116b0badaeb14d061beba0a1cd4dc4b3524
integrityrecordsadmin-wq <integrityrecordsadmin@gmail.com>
2026-07-16 12:58:39 -0500

    Update layout.js
    fixed viewport

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,14 @@ export const viewport = {
   themeColor: "#111318",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
