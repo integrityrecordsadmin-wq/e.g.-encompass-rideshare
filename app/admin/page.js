71340992206a5942e3c6f3c7a71ddfe242334b11
integrityrecordsadmin-wq <integrityrecordsadmin@gmail.com>
2026-07-20 17:46:06 -0500

    Remove empty line at the beginning of page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,4 +1,3 @@
-
 "use client";
 import { useState, useEffect } from "react";
 import { Car, User, DollarSign, Search, CheckCircle2, CircleDot, Star, AlertTriangle, X, Megaphone } from "lucide-react";
